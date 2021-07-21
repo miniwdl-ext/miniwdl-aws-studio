6d901c68ab12c9339e30938977051f7cee7eb7f6
Mike Lin <dna@mlin.net>
2021-07-21 00:22:29 -1000

    PyPI packaging

--- setup.py
+++ setup.py
@@ -1,36 +1,25 @@
 import setuptools
+from version import get_version
 
+MINIWDL_AWS_MIN_VERSION = "0.0.1"
+CDK_MIN_VERSION = "1.110.0"
 
 with open("README.md") as fp:
     long_description = fp.read()
 
-CDK_MIN_VERSION = "1.110.0"
-
 setuptools.setup(
     name="miniwdl-aws-studio",
-    version="0.0.1",
+    version=get_version(),
     description="AWS CDK app to add miniwdl+GWFCore to existing SageMaker Studio",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Wid L. Hacker",
     package_dir={"": "miniwdl_gwfcore_studio"},
     packages=setuptools.find_packages(where="miniwdl_gwfcore_studio"),
-    install_requires=["boto3"]
+    install_requires=["boto3>=1.17", f"miniwdl-aws>={MINIWDL_AWS_MIN_VERSION}"]
     + [
         f"aws-cdk.{m}>={CDK_MIN_VERSION}"
         for m in ("core", "aws_iam", "aws_ec2", "aws_efs", "cloudformation_include")
     ],
     python_requires=">=3.6",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Programming Language :: JavaScript",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Software Development :: Code Generators",
-        "Topic :: Utilities",
-        "Typing :: Typed",
-    ],
 )
